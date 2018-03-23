5508c5f40f2a606789203e49e54d7362afda1205
InfinityLoop <swsoyee@gmail.com>
2018-03-23 20:36:28 +0900

    Create Exercise-7-List-Comprehensions.py

---
+++ Exercise-7-List-Comprehensions.py
@@ -0,0 +1,12 @@
+'''
+Exercise 7: List Comprehensions
+
+Let’s say I give you a list saved in a variable:
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of
+Python that takes this list a and makes a new list that has
+only the even elements of this list in it.
+'''
+
+# Solution
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print([i for i in a if i % 2 == 0])
