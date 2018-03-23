ac818cf6328111f98f4590479cba1a5161713f17
InfinityLoop <swsoyee@gmail.com>
2018-03-23 20:31:05 +0900

    Update Exercise-6-String-Lists.py

--- Exercise-6-String-Lists.py
+++ Exercise-6-String-Lists.py
@@ -1,5 +1,5 @@
 '''
-Exercise 6:
+Exercise 6: String Lists
 Ask the user for a string and print out whether this string is a
 palindrome or not. (A palindrome is a string that reads the same
 forwards and backwards.)
