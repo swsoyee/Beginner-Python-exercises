0e8fe9d62cec7664b9d5ebb934822b3c4843975a
InfinityLoop <swsoyee@gmail.com>
2018-03-23 22:34:30 +0900

    Update and rename Exercise-7-List-Comprehensions.py to Exercise-07-List-Comprehensions.py

--- Exercise-07-List-Comprehensions.py
+++ Exercise-07-List-Comprehensions.py
@@ -5,6 +5,7 @@ Let’s say I give you a list saved in a variable:
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of
 Python that takes this list a and makes a new list that has
 only the even elements of this list in it.
+
 '''
 
 # Solution
