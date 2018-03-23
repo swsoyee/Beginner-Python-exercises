d77b869efded49363f67a301b96e3e0a9ed3c750
InfinityLoop <swsoyee@gmail.com>
2018-03-23 20:30:28 +0900

    Create Exercise-6-String-Lists.py

---
+++ Exercise-6-String-Lists.py
@@ -0,0 +1,11 @@
+'''
+Exercise 6:
+Ask the user for a string and print out whether this string is a
+palindrome or not. (A palindrome is a string that reads the same
+forwards and backwards.)
+
+'''
+
+# Solution
+input_string = input("Pleas input a string:")
+print("String is a palindrome" if input_string[::-1] == input_string else "String is not a palindrome")
